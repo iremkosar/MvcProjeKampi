7ee86dd105f67217f4c6a6eb0ee36068cb8a691d
İrem Koşar <99809817+iremkosar@users.noreply.github.com>
2025-12-16 00:05:21 +0300

    commit

---
+++ MvcProjeKampi/Controllers/MessageController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        MessageManager cm = new MessageManager(new EfMessageDal());
+        public ActionResult Inbox()
+        {
+            var messagelist = cm.GetListInbox();
+            return View(messagelist);
+        }
+        public ActionResult Sendbox()
+        {
+            var messagelist = cm.GetListSendbox();
+            return View(messagelist);
+        }
+        public ActionResult GetInBoxDetails(int id)
+        {
+            var values = cm.GetByID(id);
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+
+            return View();
+        }
+    }
+}
